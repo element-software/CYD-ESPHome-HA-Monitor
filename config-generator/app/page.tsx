d91701711dd3d8c86beb51301131f5153ad087d6
Element Software <contact@element-software.co.uk>
2026-02-19 00:22:05 +0000

    added footer with external links

--- config-generator/app/page.tsx
+++ config-generator/app/page.tsx
@@ -1,5 +1,6 @@
 import PageHeader from '@/components/PageHeader';
 import ConfigGeneratorClient from '@/components/ConfigGeneratorClient';
+import PageFooter from '@/components/PageFooter';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8">
         <PageHeader />
         <ConfigGeneratorClient />
+        <PageFooter />
       </div>
     </main>
   );
